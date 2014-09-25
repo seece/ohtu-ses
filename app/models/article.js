c4abf255d7c9fe7846ac34e2aa8f47e70f0f1f5b
Pekka V <pekka.vaananen@iki.fi>
2014-09-25 13:26:25 +0300

    update article.js

--- app/models/article.js
+++ app/models/article.js
@@ -1,29 +1,39 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// TODO load Schema json
+var ArticleSchema = new Schema({});
+
 {
-  "$schema": "http://json-schema.org/draft-03/schema#",
-  "name": "Article",
-  "type": "object",
-  "properties": {
-    "author": {
-      "type": "array",
-      "minItems": 1,
-      "items": { "type": "string" },
-      "required": true
+    author: {
+      type: array,
+      minItems: 1,
+      items: { type: string },
+      required: true
     },
-    "title": {
-      "type": "string",
-      "required": true
+    title: {
+      type: string,
+      required: true
     },
-    "year": {
-      "type": "number",
-      "required": true
+    year: {
+      type: number,
+      required: true
     },
-    "booktitle": {
-      "type": "string",
-      "required": false
+    booktitle: {
+      type: string,
+      required: false
     },
-    "publisher": {
-      "type": "string",
-      "required": false
+    publisher: {
+      type: string,
+      required: false
     }
-  }
 }
+ArticleSchema.methods = {
+}
+
+ArticleSchema.statics = {
+}
+
+mongoose.model('Article', ArticleSchema);
+
+
